d63a9f615a8b09164adfb69b36547f7bd36c8e9b
Matheus Leal <lleal.dev@gmail.com>
2024-12-01 17:28:07 -0300

    Criando a tela de login e a Tela principal

---
+++ src/main/java/util/view/RoundedPanel.java
@@ -0,0 +1,24 @@
+package util.view;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+
+public class RoundedPanel extends JPanel {
+    private int borderRadius;
+
+    public RoundedPanel(int borderRadius) {
+        this.borderRadius = borderRadius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), borderRadius, borderRadius);
+    }
+}
