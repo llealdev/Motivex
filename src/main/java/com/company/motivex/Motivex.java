dc9dfe11038e92dfcb03b2abeef9569c7e403db5
Matheus Leal <lleal.dev@gmail.com>
2024-12-01 10:37:19 -0300

    Subindo o projeto para o  repositório

---
+++ src/main/java/com/company/motivex/Motivex.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.motivex;
+
+/**
+ *
+ * @author matheus-leal
+ */
+public class Motivex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
